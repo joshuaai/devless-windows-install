cc1692e7a8eeea14957046845f8f004876cc3140
josoftme <joshuaiheji@gmail.com>
2017-02-08 18:21:18 +0000

    Install Devless on Windows using Powershell

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # Install Devless on Windows using Docker
 
+This script downloads the devless image on Docker. It does all the checks for exisitng Docker or Devless installs, and
+allows Docker run with the Virtual Machine of your choice on Windows. 
+
 ## Pre-requsites
 
-* Ensure you have Docker installed for soomth running. Get it here [Docker for Windows](https://www.docker.com/products/docker#/windows)
+* 64-bit Windows OS
 
-* Ensure Docker is running. Go to Start Menu, type 'Docker' and run it.
+* Ensure you have a virtual machine installed. Get Oracle Virtual Box here. If you will like to use Microsoft Hyper-V as
+your VM, scroll to the end to see specific instructions for using Hyper-V.  
 
 ## Install
 
@@ -17,6 +21,25 @@
 * Cd into the directory where you cloned this file and run:
 
 `./devless_windows_install.ps1`
+
+* If you start or restart the docker service after a shutdown, you need to start a docker machine and expose devless to a port manually. We'll use 4545 again:
+
+`docker-machine env --shell powershell devless | iex`
+
+`docker run -p 4545:80 eddymens/devless:latest`
+
+* Use the following command in a new tab to check the ip of the devless machine. The default is 192.168.99.100, but you should check.
+
+* Run the address on your browser to access Devless
+`192.168.99.100:4545`
+
+## Using Microsoft Hyper-V
+
+* Ensure Hyper-V is enabled. Run the Powershell as administrator and type`n`enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All`
+
+* If you have enabled Hyperv it is important you use an internal virtual switch. In Hyper-V Manager, create a Virtual Network Switch, pick type as internal and name it "Devless".
+
+* Finally in your System's Network Connections, open the properties of your active internet connection and share the connection with the "Devless" virtual network switch. This will make sure that the IP of the boot2docker VM never changes and it still has internet access.
 
 Enjoy Devless :)
 
--- devless_windows_install.ps1
+++ devless_windows_install.ps1
@@ -3,12 +3,12 @@
     Installs the Devless image and runs it for use on port 4545
 
     .DESCRIPTION
-    This runs on already installed Docker for Windows. Get it here https://www.docker.com/products/docker#/windows.
-    It checks that the Docker service is running, if not it prompts you to start it and rerun the script.
-    Checks if you already have the Devless image and starts it on port 4545, if not it installs it and starts it on the same port.
+    This script downloads the devless image on Docker. It does all the checks for exisitng Docker or Devless installs, and
+    allows Docker run with the Virtual Machine of your choice on Windows. 
 
-    When you shutdown or restart docker, use this command to expose devless on any or same port:
-    docker run -p 4545:80 eddymens/devless:latest
+    When you shutdown or restart docker, you will need to start a docker machine and expose devless to a port manually:
+    - docker-machine env --shell powershell devless | iex
+    - docker run -p 4545:80 eddymens/devless:latest
 #>
 
 Write-Host "______           _               "
@@ -18,36 +18,98 @@ Write-Host "| | | / _ \ \ / / |/ _ \/ __/ __|"
 Write-Host "| |/ /  __/\ V /| |  __/\__ \__ \\"
 Write-Host "|___/ \___| \_/ |_|\___||___/___/"
 
-Write-Host "Checking for Docker installation..."
-
 # Perform registry check to see if Docker is already installed
 [Boolean]$DockerServicePath = Test-Path HKLM:\SYSTEM\CurrentControlSet\Services\com.docker.service
-[String]$DockerService = "com.docker.service"
+[Boolean]$DockerToolboxPath = Test-Path "C:\Program Files\Docker Toolbox\Docker.exe"
+[String]$ChocoVersion = choco -v
+[Boolean]$VBPath = Test-Path "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe"
+[String]$hyperv = Get-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V-All -Online
+[Boolean]$VMWarePath = Test-Path "HKLM\SOFTWARE\Wow6432Node\VMware, Inc.\VMware Workstation\Private\UninstallFiles"
 
-if ($DockerServicePath -eq $False) {
-    #https://download.docker.com/win/stable/InstallDocker.msi
-    Write-Host "Please install Docker from https://www.docker.com/products/docker#/windows"
-    Write-Host "Exiting..."
-}
-elseif ((Get-Service com.docker.service).Status -ne "Running") {
-    Write-Host "Please ensure docker is running and run the script again"
-    Write-Host "Exiting..."
+Write-Host "Checking for Chocolatey installation..."
+if ($ChocoVersion -eq $null) {
+    #Install chocolatey package manager
+    iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
 }
 else {
-    Write-Host "Docker is installed :)"
+    Write-Host "Chocolatey already installed :)"
+    
+    Write-Host "Checking for Docker installation..."
+    if (($DockerServicePath -eq $False) -and ($DockerToolboxPath -eq $False)) {
+        Write-Host "Downloading Docker..."
+        choco install -y docker
 
-    Write-Host "Downloading the Devless image container..."
+        Write-Host "Downloading docker-machine..."
+        choco install -y docker-machine
 
-    [String]$DevlessId = docker images -q eddymens/devless
+        Write-Host "Downloading docker-composer..."
+        choco install -y docker-composer
 
-    if (($DevlessId -ne $null) -and ($DevlessId -ne '')) {
-        Write-Host "Devless image already downloaded :)"
+        #MY CODE FOR CHECKING WHICH VIRTUAL MACHINE TO USE
+        if ($hyperv -eq "Enabled") {
+            docker-machine create -d hyperv --hyperv-virtual-switch "Devless" devless
+        }
+        elseif ($VBPath -eq "True") {
+            docker-machine create -d virtualbox devless
+        }
+        elseif ($VMWarePath -eq "True") {
+            #Docker driver plugin from VMware Workstation
+            choco install docker-machine-vmwareworkstation
+            docker-machine create -d vmwareworkstation devless
+        }
+        else {
+            Write-Host "Please install a virtual machine.`nGet Virtual Box online or enable Microsoft Hyper V from
+            powershell as administrator using: enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All`n
+            Run this script again after that is done."
+        }
+
+        Write-Host "Starting the docker-machine..."
+        docker-machine env --shell powershell devless | iex
+
+        #Download the devless image
+        Write-Host "Downloading the Devless image container..."
+        docker pull eddymens/devless   
+
+        #Expose Devless to port 4545
+        [String]$DevlessId = docker images -q eddymens/devless
+        if (($DevlessId -ne $null) -and ($DevlessId -ne '')) {
+            Write-Host "Starting Devless on port 4545..."
+            docker run -p 4545:80 eddymens/devless:latest
+        } 
+        else {
+            Write-Host "Devless download failed. Running the script again with good internet connection will fix this."
+        }
+    }
+    elseif ($DockerServicePath -eq $True) {
+        Write-Host "Docker is installed :)"
+
+        Write-Host "Checking to see if Docker Service is running"
+        [String]$DockerService = "com.docker.service"
+        if ((Get-Service $DockerService).Status -ne "Running") {
+            Write-Host "Please ensure docker is running and run the script again"
+            Write-Host "Exiting..."
+        }
+        else {
+            #Download the devless image
+            Write-Host "Downloading the Devless image container..."
+            docker pull eddymens/devless   
+
+            [String]$DevlessId = docker images -q eddymens/devless
+            if (($DevlessId -ne $null) -and ($DevlessId -ne '')) {
+                #Expose Devless to port 4545
+                Write-Host "Starting Devless on port 4545..."
+                docker run -p 4545:80 eddymens/devless:latest
+            } 
+            else {
+                Write-Host "Devless download failed. Running the script again with good internet connection will fix this."
+            }
+        }
     }
     else {
-        docker pull eddymens/devless   
+        Write-Host "You have Docker Toolbox already installed.`nPlease open you Docker Quickstart Terminal and
+        type this: docker pull eddymens/devless.`nAfter the image is pulled, expose devless to a port by typing: 
+        docker run -p 4545:80 eddymens/devless:latest.`nCheck the host of the running docker machine (default is
+        192.168.99.100) using the docker command: docker-machine ls.`nOn your browser, enter 192.168.99.100:4545 to
+        see Devless.`nEnjoy Devless :)"
     }
-
-    Write-Host "Starting Devless on port 4545"
-
-    docker run -p 4545:80 eddymens/devless:latest 
-} 
+}
